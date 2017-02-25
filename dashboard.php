eb3645934d32f161a63f58863d71d0f64125b9e8
elpacko <elpacko@gmail.com>
2017-02-25 14:18:18 -0700

    function para insertar
    se agrega función para insertar un articulo

--- dashboard.php
+++ dashboard.php
@@ -1,12 +1,14 @@
 
 <?php
 include("conn.php");
-echo print_r($_POST);
+//echo print_r($_POST);
 if($_POST['setaction']=='addItem')
 {
 
-  echo "insert into articulos (NombreArticulo,Tipo,InventarioMaximo,InventarioMinimo,inventario,ultimaCompra,siguienteExpirar) values ('".$_POST['setaction']."','".$_POST['setaction']."',".$_POST['setaction'].",".$_POST['setaction'].",".$_POST['setaction'].",'".$_POST['setaction']."','".$_POST['setaction']."');";
+ 
 
+   $sqlpaquetes= "insert into articulos (NombreArticulo,Tipo,InventarioMaximo,InventarioMinimo,inventario,ultimaCompra,siguienteExpirar) values ('".$_POST['NombreArticulo']."','".$_POST['Tipo']."',".$_POST['InventarioMaximo'].",".$_POST['InventarioMinimo'].",".$_POST['inventario'].", CURDATE(),STR_TO_DATE('".$_POST['siguienteExpirar']."', '%m/%d/%Y'));";
+        $respaquetes=mysql_query($sqlpaquetes);
 }
   ?>
   <!DOCTYPE html>
@@ -104,70 +106,7 @@ if($_POST['setaction']=='addItem')
 
     <!-- Main content -->
     <section class="content">
-      <!-- Small boxes (Stat box) -->
-      <div class="row">
-        <div class="col-lg-3 col-xs-6">
-          <!-- small box -->
-          <div class="small-box bg-aqua">
-            <div class="inner">
-              <h3>150</h3>
-
-              <p>New Orders</p>
-            </div>
-            <div class="icon">
-              <i class="ion ion-bag"></i>
-            </div>
-            <a href="#" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
-          </div>
-        </div>
-        <!-- ./col -->
-        <div class="col-lg-3 col-xs-6">
-          <!-- small box -->
-          <div class="small-box bg-green">
-            <div class="inner">
-              <h3>53<sup style="font-size: 20px">%</sup></h3>
-
-              <p>Bounce Rate</p>
-            </div>
-            <div class="icon">
-              <i class="ion ion-stats-bars"></i>
-            </div>
-            <a href="#" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
-          </div>
-        </div>
-        <!-- ./col -->
-        <div class="col-lg-3 col-xs-6">
-          <!-- small box -->
-          <div class="small-box bg-yellow">
-            <div class="inner">
-              <h3>44</h3>
-
-              <p>User Registrations</p>
-            </div>
-            <div class="icon">
-              <i class="ion ion-person-add"></i>
-            </div>
-            <a href="#" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
-          </div>
-        </div>
-        <!-- ./col -->
-        <div class="col-lg-3 col-xs-6">
-          <!-- small box -->
-          <div class="small-box bg-red">
-            <div class="inner">
-              <h3>65</h3>
-
-              <p>Unique Visitors</p>
-            </div>
-            <div class="icon">
-              <i class="ion ion-pie-graph"></i>
-            </div>
-            <a href="#" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
-          </div>
-        </div>
-        <!-- ./col -->
-      </div>
-      <!-- /.row -->
+    
 
 
           <!-- TO DO List -->
